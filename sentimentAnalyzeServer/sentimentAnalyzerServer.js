8f3ef11dba7842094eab25d39e2b7748d5473681
rockmanck <rockman.roll@gmail.com>
2021-04-04 17:01:36 +0000

    added nlu

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv');
+dontenv.config();
+
+getNLUInstance = () => {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NLUv1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const nlu = new NLUv1({
+        version: "2020-08-01",
+        authenticator: new IamAuthenticator({
+            apikey: api_key
+        }),
+        serviceUrl: api_url
+    });
+    return nlu;
+}
 
 app.use(express.static('client'))
 
