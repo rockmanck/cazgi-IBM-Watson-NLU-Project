3a9f61f1df27d5ba6811e5b353cf438cb2e78559
rockmanck <rockman.roll@gmail.com>
2021-04-05 19:17:16 +0000

    added init script reminder

---
+++ sentimentAnalyzeServer/init-order.sh
@@ -0,0 +1,13 @@
+### server
+git clone https://github.com/rockmanck/cazgi-IBM-Watson-NLU-Project.git
+
+#create .env file with API_KEY and API_URL
+
+npm install --save
+npm install nodemon --save-dev
+npm install --save ibm-watson
+npm install --save dotenv
+
+### client
+npm install --save
+npm run-script build
